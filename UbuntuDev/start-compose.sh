f6d3cc40165327b6f85ea1a9e38b20719761b0ac
Billion Florian <Florian@MacBook-Pro-de-Billion.local>
2018-06-08 11:46:12 +0200

    Modif Ubundev B1

--- UbuntuDev/start-compose.sh
+++ UbuntuDev/start-compose.sh
@@ -1,3 +1,3 @@
 # /bin/bash
-[ $1 == "" ] && exit 1
-docker run --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup -v /home/loick/Bureau/Docker/Cours/Projet/UbuntuDev/data/$1:/home/$1 -P --name ubuntunssh ubuntu:1.0
+[ "$1" == "" ] && exit 1
+docker run --rm --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup -v /Users/Florian/Desktop/Cours/B3/Docker/Docker/UbuntuDev/data/$1:/data/home/$1 -P --name $1 aveylon/projet_b3_docker:ubuntu-b1
