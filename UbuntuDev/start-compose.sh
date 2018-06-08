dc3db5ef6510657f3231240a458f8cd25c3d0016
Billion Florian <Florian@MacBook-Pro-de-Billion.local>
2018-06-08 12:26:31 +0200

    Modif UbuntuDev/startup-compose.yml

--- UbuntuDev/start-compose.sh
+++ UbuntuDev/start-compose.sh
@@ -1,3 +1,3 @@
 # /bin/bash
 [ "$1" == "" ] && exit 1
-docker run --rm --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup -v /Users/Florian/Desktop/Cours/B3/Docker/Docker/UbuntuDev/data/$1:/data/home/$1 -P --name $1 aveylon/projet_b3_docker:ubuntu-b1
+docker run --rm --privileged -d -v /sys/fs/cgroup:/sys/fs/cgroup -v /data/home/$1:/data/home/$1 -P --name $1 aveylon/projet_b3_docker:ubuntu-b1
